6c697e9c9109ea7c9eb30824b9913e6b41363d3d
AlexLeonardos <27leonardos@gmail.com>
2024-11-11 01:49:02 -0500

    Implemented the Input Datas, Output Datas and Interactors for each Seeding Use Case

--- src/main/java/use_case/update_seeding/UpdateSeedingInputData.java
+++ src/main/java/use_case/update_seeding/UpdateSeedingInputData.java
@@ -1,4 +1,32 @@
 package use_case.update_seeding;
 
+import java.util.List;
+
+/**
+ * The Input Data for the Updating Seeding Locally Use Case.
+ */
 public class UpdateSeedingInputData {
+
+    private int oldSeed;
+    private int newSeed;
+    private int maxSeed;
+
+    public UpdateSeedingInputData(int oldSeed, int newSeed, int maxSeed) {
+        this.oldSeed = oldSeed;
+        this.newSeed = newSeed;
+        this.maxSeed = maxSeed;
+    }
+
+    public int getOldSeed() {
+        return oldSeed;
+    }
+
+    public int getNewSeed() {
+        return newSeed;
+    }
+
+    public int getMaxSeed() {
+        return maxSeed;
+    }
+
 }
