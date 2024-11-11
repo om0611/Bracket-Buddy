21014bb372f60640249477738a0601e3953b9c7b
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-11-11 00:24:23 -0500

    Implement Presenters

---
+++ src/main/java/interface_adapter/main/MainState.java
@@ -0,0 +1,4 @@
+package interface_adapter.main;
+
+public class MainState {
+}
--- src/main/java/interface_adapter/update_seeding/SeedingState.java
+++ src/main/java/interface_adapter/update_seeding/SeedingState.java
@@ -1,15 +1,41 @@
 package interface_adapter.update_seeding;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
 
 /**
  * A state containing all the data used for seeding
  */
 public class SeedingState {
     private List<Integer> seeding;
+    private SortedMap<String, Integer> phaseToID;
+    private String error = "";
 
     public void setSeeding(List<Integer> newSeeding) {
         seeding = newSeeding;
+    }
+
+    public void setPhases(SortedMap<String, Integer> newPhases) {
+        phaseToID = newPhases;
+    }
+
+    /**
+     * Get phase names.
+     * @return Names of phases
+     */
+    public Set<String> getPhases() {
+        return phaseToID.keySet();
+    }
+
+    /**
+     * Converts from phase name to id.
+     * @param phase Name of the phase
+     * @return ID of the phase
+     */
+    public int phaseNametoId(String phase) {
+        return phaseToID.get(phase);
     }
 
     public List<Integer> getSeeding() {
@@ -26,5 +52,9 @@ public class SeedingState {
         int tempID = seeding.get(oldSeed - 1);
         seeding.remove(oldSeed - 1);
         seeding.add(newSeed - 1, tempID);
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 }
