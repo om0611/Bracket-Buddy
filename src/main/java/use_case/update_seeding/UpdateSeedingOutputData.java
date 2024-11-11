a15fd1e8329138ce5dbae932965b8cac652a3693
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-11-11 01:11:31 -0500

    Implement output data class for updating seeding

--- src/main/java/use_case/update_seeding/UpdateSeedingOutputData.java
+++ src/main/java/use_case/update_seeding/UpdateSeedingOutputData.java
@@ -1,4 +1,22 @@
 package use_case.update_seeding;
 
+/**
+ * Output Data for the Update Seeding Use Case.
+ */
 public class UpdateSeedingOutputData {
+    private int oldSeed;
+    private int newSeed;
+
+    public UpdateSeedingOutputData(int oldSeed, int newSeed) {
+        this.oldSeed = oldSeed;
+        this.newSeed = newSeed;
+    }
+
+    public int getOldSeed() {
+        return oldSeed;
+    }
+
+    public int getNewSeed() {
+        return newSeed;
+    }
 }
