a311f64069b3091ab5869aea9bf9bf2dce594cd7
Om Patel <ompatel.patel@mail.utoronto.ca>
2024-11-11 01:58:35 -0500

    added UpdateSeeding controller

--- src/main/java/interface_adapter/update_seeding/UpdateSeedingController.java
+++ src/main/java/interface_adapter/update_seeding/UpdateSeedingController.java
@@ -1,4 +1,24 @@
 package interface_adapter.update_seeding;
 
+import use_case.update_seeding.UpdateSeedingInputData;
+import use_case.update_seeding.UpdateSeedingInputBoundary;
+import interface_adapter.update_seeding.SeedingState;
+
 public class UpdateSeedingController {
+
+    private final UpdateSeedingInputBoundary updateSeedingUseCaseInteractor;
+    private final SeedingState seedingState;
+
+    public UpdateSeedingController(UpdateSeedingInputBoundary updateSeedingUseCaseInteractor, SeedingState seedingState) {
+        this.updateSeedingUseCaseInteractor = updateSeedingUseCaseInteractor;
+        this.seedingState = seedingState;
+    }
+
+    public void execute(int oldSeed, int newSeed) {
+
+        int maxSeed = seedingState.getSeeding().size();
+        final UpdateSeedingInputData updateSeedingInputData = new UpdateSeedingInputData(oldSeed, newSeed, maxSeed);
+
+        updateSeedingUseCaseInteractor.execute(updateSeedingInputData);
+    }
 }
