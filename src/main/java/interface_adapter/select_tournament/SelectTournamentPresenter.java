32440a200c1b69e049098e6dc2ad21be9f081f32
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-11-27 00:27:22 -0500

    Removed old src folder

--- src/main/java/interface_adapter/select_tournament/SelectTournamentPresenter.java
+++
@@ -1,4 +0,0 @@
-package interface_adapter.select_tournament;
-
-public class SelectTournamentPresenter {
-}
--- src/main/java/use_case/select_tournament/SelectTournamentOutputData.java
+++
@@ -1,4 +0,0 @@
-package use_case.select_tournament;
-
-public class SelectTournamentOutputData {
-}
